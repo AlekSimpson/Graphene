e3af4a6fc0113fb3d653d13852693a75bf9fef5e
AlekSimpson <alek@asu.me>
2021-11-08 19:00:13 -0700

    fixed all svg generation bugs

--- graphene.py
+++ graphene.py
@@ -14,60 +14,48 @@ class Point:
     def printSelf(self):
         print(self.coords)
     
+class Line:
+    def __init__(self, glyph, vector, initPoint):
+        self.glyph = glyph 
+        self.mod = -0.5 if vector[0] > 0 else 0.5 
+        self.vector = (vector[0], vector[1] * -1)
+        self.initPoint = initPoint
+        self.destPoint = self.get_dest_point()
+        self.initPoint = Point((initPoint.x + self.mod, initPoint.y), self.glyph) 
+        self.draw_self()
+
+    def get_dest_point(self):
+        dest = (self.initPoint.x + self.vector[0] + self.mod, self.initPoint.y + self.vector[1])
+        destPoint = Point(dest, self.glyph)
+        return destPoint
+
+    def draw_self(self):
+        self.initPoint.drawTo(self.destPoint)
+
 class Glyph:
-    def __init__(self, filename='test.svg', size=('50px', '50px')):
-        self.g = svgwrite.Drawing(filename, size)
-        self.last_point = self.get_center(size)
-
-    def parse_size(self, size):
-        n = size[0].replace('p', '')
-        new_string = n.replace('x', '')
-        return new_string
-
-    def get_center(self, size):
-        width = self.parse_size(size)
-        width = int(width)
-        half = width / 2
-
-        center = (half, half)
-        center_point = Point(center, self)
-        return center_point
-
-    def init_point(self, coords):
-        p = Point(coords, self)
-        return p 
-
-    def invert_stroke_vector(self, vector):
-        new = (vector[0], vector[1] * -1)
-        return new
+    def __init__(self, filename='test.svg', size=50):
+        s = str(size) + "px"
+        self.filename = filename
+        self.size = (s, s)
+        self.g = svgwrite.Drawing(self.filename, self.size)
+        center_num = (size / 2)
+        self.last_point = Point((center_num, center_num), self)
 
     def stroke(self, vector):
-        new_vec = self.invert_stroke_vector(vector)
-        init_point = self.init_point((self.last_point.x + 0.5, self.last_point.y))
-        if new_vec[0] > 0:
-            init_point = self.init_point((self.last_point.y + 0.5, self.last_point.y))
-
-        n = (init_point.x + new_vec[0], init_point.y + new_vec[1])
-        new_point = Point(n, self)
-
-        init_point.drawTo(new_point)
-        self.last_point = new_point
+        line = Line(self, vector, self.last_point)
+        self.last_point = line.destPoint 
 
     def end(self):
         self.g.save()
 
-    # def cirlce(self):
-    #     c = self.g.circle(center=(15*cm, 8*cm), r='2.5cm', stroke='blue', stroke_width=3)
-    #     self.g.add(c)
-    
-    def printPoints(self):
-        for p in self.points:
-            p.printSelf()
+    def cirlce(self):
+         c = self.g.circle(center=(15*cm, 8*cm), r='2.5cm', stroke='blue', stroke_width=3)
+         self.g.add(c)
 
-class GlyphBlock(Glyph):
-    def __init__(self, glyphs=2, gFilename='test.svg', gSize=('50px', '50px')):
-        Glyph.__init__(self, gFilename, gSize)
-        self.glyphs = glyphs
+# class GlyphBlock(Glyph):
+#     def __init__(self, glyphs=2, gFilename='test.svg', gSize=('50px', '50px')):
+#         Glyph.__init__(self, gFilename, gSize)
+#         self.glyphs = glyphs
 
-    def create_quadrants(self):
-        print('work in progress')
+#     def create_quadrants(self):
+#         print('work in progress')
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from graphene import Glyph
 
 def createGlyph():
     g = Glyph()
-
+    
     g.stroke((0, 10))
-
+    
     g.stroke((-15, 0))
-
+    
     g.stroke((0, -10))
-
+    
     g.stroke((15, 0))
  
     g.end()
